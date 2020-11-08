fa6e4982a2121d9a378c48578d3dd3417ccb9941
JiayingHuang1 <644980597@qq.com>
2020-11-08 20:50:02 +0800

    Initial commit

---
+++ writable/data/data.php
@@ -0,0 +1,40 @@
+  
+<root>
+<record>
+    <id>1</id>
+    <name>Hong Kong</name>
+    <description>A special administrative region on the eastern side of the Pearl River estuary in southern China.</description>
+    <image>'hongkong.jpg</image>
+</record>
+
+<record>
+    <id>2</id>
+    <name>Macau</name>
+    <description>A special administrative region on the western side of the Pearl River estuary in southern China.</description>
+    <image>macau.jpg</image>
+</record>
+
+
+<record>
+    <id>3</id>
+    <name>Thailand</name>
+    <description>A country at the centre of the Southeast Asian Indochinese peninsula composed of 76 provinces.</description>
+    <image>thailand.jpg</image>
+</record>
+
+<record>
+    <id>4</id>
+    <name>Japan</name>
+    <description>An island country in East Asia.</description>
+    <image>japan.jpg</image>
+</record>
+
+<record>
+    <id>5</id>
+    <name>Vietnam</name>
+    <description>The easternmost country on the Indochina Peninsula.</description>
+
+    <image>vietnam.jpg</image>
+</record>
+
+</root>
